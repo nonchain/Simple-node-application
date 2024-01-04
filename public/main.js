5f53bafee57387fb9ebc7abc61737078764d4cfa
nonchain <code.progali@gmail.com>
2024-01-04 16:15:04 +0330

    Navigate user to home page

--- public/main.js
+++ public/main.js
@@ -1,14 +1,22 @@
 const btnAddFriend = document.querySelector(".button-confirm");
 const inputNewFriend = document.querySelector('input[name="name"]');
 
-btnAddFriend.addEventListener("click", () => {
-  fetch("http://localhost:3001/friends", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      "name": inputNewFriend.value
-    })
-  })
-})
+btnAddFriend &&
+  btnAddFriend.addEventListener("click", () => {
+    fetch("http://localhost:3001/friends", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: inputNewFriend.value,
+      }),
+    }).then(() => {
+      const element = document.createElement("a");
+      element.setAttribute("href", "http://localhost:3001/");
+      element.style.display = "none";
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    });
+  });
